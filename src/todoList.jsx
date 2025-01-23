8c2fb7d519eca927d45ed0426046486b1f886a7c
jsimpsonux <jsimpsonux@gmail.com>
2025-01-23 14:28:13 +0000

    Added basic structure for TodoList component

---
+++ src/todoList.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+function TodoList() {
+    return (
+        <div>
+            <h2>My To-Do List</h2>
+            {/* Add input and buttons here */}
+            {/* Render tasks here */}
+        </div>
+    );
+}
+
+export default TodoList;
